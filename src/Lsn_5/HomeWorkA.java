83b8f967ee1fa44362c013f0c9083f613550e892
RashadGr <rashad.tgr@gmail.com>
2023-04-14 01:02:33 +0400

    The final state of the home works: A / B

---
+++ src/Lsn_5/HomeWorkA.java
@@ -0,0 +1,43 @@
+package Lsn_5;
+
+/**
+ * The user is prompted to enter integers into an array, and the program
+ * will subsequently calculate and print the sum and average of the array elements.
+ */
+
+import java.util.Scanner;
+
+public class HomeWorkA {
+    public static void main(String[] args) {
+
+        // Local variables
+        int sumArray = 0;
+        float averageArray = 0;
+
+        // Declaring array and allocating memory
+        int userInput[] = new int[10];
+        int len = userInput.length;
+
+        // Prompt the user to input integers into the array
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter " + len + " int numbers: ");
+
+        // Get inputs for array; calculate Sum and Average of all the elements
+        for (int i = 0; i < len; i++){
+            userInput[i] = scan.nextInt();
+            sumArray += userInput[i];
+            averageArray = sumArray/len;
+        }
+
+        // Print to console in array format
+        System.out.print("userInput[] = {");
+        for (int j = 0; j < len; j++){
+            String comma = (j != len - 1) ? ", " : "";
+            System.out.print(userInput[j] + comma);
+        }
+        System.out.println("}");
+        System.out.println("The SUM of array elements: " + sumArray);
+        System.out.println("The AVERAGE of array elements: " + averageArray);
+
+    }
+}
---
+++ src/Lsn_5/HomeWorkB.java
@@ -0,0 +1,28 @@
+package Lsn_5;
+
+/**
+ * This program prints out the values in the array in reverse order.
+ */
+
+public class HomeWorkB {
+    public static void main(String[] args) {
+
+        int originalArray[] = {3, 6, 9, 12, 15, 17, 77};
+        int len = originalArray.length;
+        int reversedArray[] = new int[len];
+
+        System.out.print("reversedArray[] = {");
+
+        // Iterate in reverse order
+        for (int i = len - 1; i >= 0; i--) {
+            reversedArray[i] = originalArray[i]; // assign each element of the original arr to the reversed arr
+            System.out.print(reversedArray[i]);
+
+            // Print in array format & avoid comma after the last element
+            String format = (i != 0) ? ", " : "";
+            System.out.print(format);
+        }
+        System.out.print("}");
+
+    }
+}
