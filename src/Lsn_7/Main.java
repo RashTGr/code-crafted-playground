b4cc0ef485c62886abdfebdfec7f39e6d821e4c7
RashadGr <rashad.tgr@gmail.com>
2023-04-26 13:30:33 +0400

    Final state of the home assignment

---
+++ src/Lsn_7/Main.java
@@ -0,0 +1,22 @@
+package Lsn_7;
+
+public class Main {
+    public static void main(String[] args) {
+        // A class instance is created, and then the method is called on that instance
+        Person man = new Person("David", "Malan", 35, "dm@example.com", "+019990000");
+
+        man.setAge(18);
+        man.setFirstName("Dave");
+        String manName = man.getFirstName();
+        String manSurname = man.getLastName();
+        int manAge = man.getAge();
+        String manEmail = man.getEmail();
+        String manPhone = man.getPhone();
+        boolean isAdult = man.isAdult();
+
+        System.out.println("isAdult: " + isAdult + "\nName, Surname: " + manName + " "
+                + manSurname + "\nAge: " + manAge + "\nE: "
+                + manEmail + "\nCell: " + manPhone);
+
+    }
+}
--- src/Lsn_7/Person.java
+++ src/Lsn_7/Person.java
@@ -2,4 +2,69 @@ package Lsn_7;
 
 public class Person {
 
+    // Attributes
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String phone;
+
+    // Constructor
+    // and constructor parameters don't have to be the same as the names of the instance variables(name, age, sound).
+    public Person(String firstN, String lastN, int age, String email, String mobile) {
+        this.firstName = firstN;
+        this.lastName = lastN;
+        this.age = age;
+        this.email = email;
+        this.phone = mobile;
+    }
+
+    // Method that sets the first name
+    // The names of the method parameters can be different from the instance variables
+    public void setFirstName(String personFirstName) {
+        this.firstName = personFirstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public void setPhone (String phone) {
+        this.phone = phone;
+    }
+
+    // Using "this" keyword with getter methods is NOT necessary, however,
+    // it CAN be a helpful practice for clarity and consistency in the code.
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone () {
+        return phone;
+    }
+
+    public boolean isAdult() {
+        return age >= 18;
+    }
+
 }
